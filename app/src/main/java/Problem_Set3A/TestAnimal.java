306695c79a6f1d5baaac544dc06d03d6774c9f15
Benny <bennyrobot1143@gmail.com>
2024-04-01 20:55:24 +0800

    Add TestAnimals for Problem Set 3A

--- app/src/main/java/Problem_Set3A/TestAnimal.java
+++ app/src/main/java/Problem_Set3A/TestAnimal.java
@@ -1,27 +1,25 @@
 package Problem_Set3A;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class TestAnimal {
+
     public static void main(String[] args) {
-        TestAnimal ta = new TestAnimal();
-        Animal cat1 = ta.new Cat("ichika");
+
+        Animal cat1 = new Cat("ichika");
         System.out.println(cat1.getName());
         System.out.println(cat1.makeSound());
         /** Output:
          * Cat:ichika
          * Cat:ichika says Meow */
 
-        Animal cat2 = ta.new Cat("nino");
-        Animal cat3 = ta.new Cat("ichika");
+        Animal cat2 = new Cat("nino");
+        Animal cat3 = new Cat("ichika");
         System.out.println(cat1.equals(cat3));
         System.out.println(cat2.equals(cat3));
         /** Output:
          * true
          * false */
 
-        Animal dog1 = ta.new Dog("Fido");
+        Animal dog1 = new Dog("Fido");
         System.out.println(dog1.getName());
         System.out.println(dog1.makeSound());
         /** Output
@@ -29,116 +27,45 @@ public class TestAnimal {
          * Dog:Fido says Woof
          */
     }
-    public class Cat extends Animal{
+}
 
-        public Cat(String name) {
-            super("Cat:" + name);
-        }
 
-        @Override
-        public String makeSound() {
-            return getName() + " says Meow";
-        }
-    }
-    public class Cow extends Animal{
 
-        public Cow(String name) {
-            super("Cow:" + name);
-        }
+abstract class Animal {
 
-        @Override
-        public String makeSound() {
-            return getName() + " says Moo Moo";
-        }
-    }
-    public class Dog extends Animal{
+    private String name;
 
-        public Dog(String name) {
-            super("Dog:" + name);
-        }
-
-        @Override
-        public String makeSound() {
-            return getName() + " says Woof";
-        }
-    }
-    public abstract class Animal {
-
-        private String name;
-
-        Animal(String name){
-            this.name = name;
-        }
-
-        public String getName(){
-            return name;
-        }
-
-        public abstract String makeSound();
-
-        // override toString(), equals() and hashCode()
-        @Override
-        public String toString() {
-            return getName();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Animal animal = (Animal) o;
-            return Objects.equals(name, animal.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name);
-        }
+    Animal(String name){
+        this.name = name;
     }
 
-    public interface AnimalFactory {
-        Animal createAnimal( String type, String name);
+    public String getName(){
+        return name;
     }
 
-    public class FarmFactory implements AnimalFactory{
-        public Animal createAnimal(String type, String name){
-            switch(type) {
-                case "Cat": return new Cat(name);
-                case "Dog": return new Dog(name);
-                case "Cow": return new Cow(name);
-                default: {
-                    return null;
-                }
-            }
-        }
-    }
-    public class Zoo {
+    public abstract String makeSound();
 
-        private AnimalFactory animalFactory;
-        private List<Animal> animalList;
-
-        Zoo(AnimalFactory animalFactory){
-            this.animalFactory = animalFactory;
-            animalList = new ArrayList<>();
-        }
-
-        public void addAnimal(String type, String name){
-            Animal a = animalFactory.createAnimal(type, name);
-            if(a != null) animalList.add(a);
-        }
-
-        public String performConcert(){
-            String str = "";
-            for(Animal a: animalList) {
-                str += a.makeSound() + "*";
-            }
-            return str;
-        }
-
-        @Override
-        public String toString() {
-            return animalList.toString();
-        }
-    }
+    // override toString(), equals() and hashCode()
 
 }
+
+class Dog extends Animal {
+    Dog(String name) {
+        super("Dog: " + name);
+    }
+    @Override
+    public String makeSound() {
+        return "";
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name) {
+        super("Cat: " + name);
+    }
+    @Override
+    public String makeSound() {
+        return "";
+    }
+}
+
